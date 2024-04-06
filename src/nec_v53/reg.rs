4e158b5525e67b8d58393eb6f34ee8b9c46537a6
Adam A <adam@hack.bg>
2024-04-06 05:24:49 +0300

    Generate PUSH implementations in register macros.
    Also gonna be easy to generate corresponding POP instructions.
    
    Wondering about the factoring between the two kinds of
    register macros, though. There's some repetition of
    implementation, while the main difference is only
    whether there are separate high/low byte registers
    (which is true of AW-DW but not the rest).
    
    So maybe that feature should be a separate macro,
    (`define_byte_register_pair!`), while the current macros
    for "general-purpose" and "special" registers should be
    unified (`define_word_register!`)

--- src/nec_v53/reg.rs
+++ src/nec_v53/reg.rs
@@ -11,6 +11,7 @@ macro_rules! define_general_purpose_register {
         $mov_l_i:ident
         $inc:ident
         $dec:ident
+        $push:ident
     ) => {
         $(#[$attr])*
         impl CPU {
@@ -78,6 +79,13 @@ macro_rules! define_general_purpose_register {
             state.set_v(signed_overflow);
             2
         }
+
+        #[inline]
+        pub fn $push (state: &mut CPU) -> u64 {
+            state.set_sp(state.sp() - 2);
+            state.write_u16(state.sp(), state.$w());
+            if state.sp() % 2 == 0 { 5 } else { 9 }
+        }
     }
 }
 
@@ -97,7 +105,7 @@ define_general_purpose_register!(
     ///   - Data exchange
     aw set_aw ah set_ah al set_al
     mov_aw_i mov_ah_i mov_al_i
-    inc_aw dec_aw
+    inc_aw dec_aw push_aw
 );
 
 define_general_purpose_register!(
@@ -107,7 +115,7 @@ define_general_purpose_register!(
     ///   - Data exchange (table reference)
     bw set_bw bh set_bh bl set_bl
     mov_bw_i mov_bh_i mov_bl_i
-    inc_bw dec_bw
+    inc_bw dec_bw push_bw
 );
 
 define_general_purpose_register!(
@@ -122,7 +130,7 @@ define_general_purpose_register!(
     ///   - BCD operation
     cw set_cw ch set_ch cl set_cl
     mov_cw_i mov_ch_i mov_cl_i
-    inc_cw dec_cw
+    inc_cw dec_cw push_cw
 );
 
 define_general_purpose_register!(
@@ -133,7 +141,7 @@ define_general_purpose_register!(
     ///   - Indirect addressing input/output
     dw set_dw dh set_dh dl set_dl
     mov_dw_i mov_dh_i mov_dl_i
-    inc_dw dec_dw
+    inc_dw dec_dw push_dw
 );
 
 macro_rules! define_special_register {
@@ -143,6 +151,7 @@ macro_rules! define_special_register {
         $mov:ident
         $inc:ident
         $dec:ident
+        $push:ident
     ) => {
         $(#[$attr])*
         impl CPU {
@@ -184,48 +193,55 @@ macro_rules! define_special_register {
             state.set_v(signed_overflow);
             2
         }
+
+        #[inline]
+        pub fn $push (state: &mut CPU) -> u64 {
+            state.set_sp(state.sp() - 2);
+            state.write_u16(state.sp(), state.$w());
+            if state.sp() % 2 == 0 { 5 } else { 9 }
+        }
     }
 }
 
 define_special_register!(
     /// The PS register contains the location of the program segment.
-    ps  set_ps   mov_ps      inc_ps  dec_ps
+    ps  set_ps   mov_ps      inc_ps  dec_ps  push_ps
 );
 define_special_register!(
     /// The SS register contains the location of the stack segment.
-    ss  set_ss   mov_ss      iec_ss  dec_ss
+    ss  set_ss   mov_ss      iec_ss  dec_ss  push_ss
 );
 define_special_register!(
     /// The DS0 register contains the location of data segment 0.
-    ds0 set_ds0  mov_ds0_i   inc_ds0 dec_ds0
+    ds0 set_ds0  mov_ds0_i   inc_ds0 dec_ds0 push_ds0
 );
 define_special_register!(
     /// The DS1 register contains the location of data segment 1.
-    ds1 set_ds1  mov_ds1_i   inc_ds1 dec_ds1
+    ds1 set_ds1  mov_ds1_i   inc_ds1 dec_ds1 push_ds1
 );
 define_special_register!(
     /// The stack pointer register.
-    sp  set_sp   mov_sp_i    inc_sp  dec_sp
+    sp  set_sp   mov_sp_i    inc_sp  dec_sp  push_sp
 );
 define_special_register!(
     /// The block pointer register.
-    bp  set_bp   mov_bp_i    inc_bp  dec_bp
+    bp  set_bp   mov_bp_i    inc_bp  dec_bp  push_bp
 );
 define_special_register!(
     /// The program counter register.
-    pc  set_pc   mov_pc_i    inc_pc  dec_pc
+    pc  set_pc   mov_pc_i    inc_pc  dec_pc  push_pc
 );
 define_special_register!(
     /// The IX register.
-    ix  set_ix   mov_ix_i    inc_ix  dec_ix
+    ix  set_ix   mov_ix_i    inc_ix  dec_ix  push_ix
 );
 define_special_register!(
     /// The IY register.
-    iy  set_iy   mov_iy_i    inc_iy  dec_iy
+    iy  set_iy   mov_iy_i    inc_iy  dec_iy  push_iy
 );
 define_special_register!(
     /// The PSW (program status word) register contains flags.
-    psw set_psw  mov_psw_i   inc_psw dec_psw
+    psw set_psw  mov_psw_i   inc_psw dec_psw push_psw
 );
 
 #[inline]
@@ -250,62 +266,6 @@ pub fn clr1_dir (state: &mut CPU) -> u64 {
 pub fn set1_dir (state: &mut CPU) -> u64 {
     state.set_dir(true);
     2
-}
-
-#[inline]
-pub fn push_ds0 (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.ds0());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_ds1 (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.ds1());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_aw (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.aw());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_bw (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.bw());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_cw (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.cw());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_dw (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.dw());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_ix (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.ix());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
-}
-
-#[inline]
-pub fn push_iy (state: &mut CPU) -> u64 {
-    state.set_sp(state.sp() - 2);
-    state.write_u16(state.sp(), state.iy());
-    if state.sp() % 2 == 0 { 5 } else { 9 }
 }
 
 #[inline]
