3e1ea468914a0dea0b2144b510d2e9459d317ad9
Adam A <adam@hack.bg>
2024-04-28 23:40:19 +0300

    Fix errors in web crate

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -1,5 +1,8 @@
+extern crate js_sys;
 extern crate wasm_bindgen;
 extern crate mpcemu_v53;
+
+use wasm_bindgen::prelude::*;
 
 use js_sys::{
     Uint8Array,
@@ -12,25 +15,25 @@ pub struct V53(mpcemu_v53::CPU);
 #[wasm_bindgen]
 impl V53 {
     #[wasm_bindgen(constructor)]
-    pub async fn new (rom: Uint8Array) -> Result<API, Error> {
+    pub fn new (rom: Uint8Array) -> Result<V53, Error> {
         console_error_panic_hook::set_once();
-        let mem: Vec<u8> = vec![0u8; rom.length() as usize];
-        rom.copy_to(mem);
+        let mut mem: Vec<u8> = vec![0u8; rom.length() as usize];
+        rom.copy_to(&mut mem);
         Ok(Self(mpcemu_v53::CPU::new(mem)))
     }
 
     #[wasm_bindgen]
-    fn step () {
+    pub fn step () {
         unimplemented!()
     }
 
     #[wasm_bindgen]
-    fn read_state () {
+    pub fn read_state () {
         unimplemented!()
     }
 
     #[wasm_bindgen]
-    fn read_memory () {
+    pub fn read_memory () {
         unimplemented!()
     }
 }
